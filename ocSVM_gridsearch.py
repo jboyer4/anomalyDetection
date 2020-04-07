90c1016f62c17aa95a5fdd0a3c67aebbbbe6edf1
Justin Boyer <justinbboyer@gmail.com>
2020-04-07 13:15:29 -0700

    rewrote pipeline
    I split several operations out into functions and updated the scatterplot figure

---
+++ ocSVM_gridsearch.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 13:22:15 2020
+
+@author: Justin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as sk
+import sklearn.model_selection
+import sklearn.svm
+import pandas as pd
+import math
+# Seaborn visualization library
+import seaborn as sns
+
+
+#splitData(data: Iris data as pandas data frame, alpha: % of training data to be anomaly as decimal, nTrain: % of nominal points to use for training as decimal)
+#Because the iris dataset is three class not one class, we need to convert it to a one class dataset in order to use it as sample data for anomaly detection with ocsvm
+#Iris setosa is considered anomlaous (-1) with versicolor and virginica considered nominal (1).
+#
+#returns: Two pandas dataframes - one to be used for training the data and the other for use as a testing set
+def splitData(data, alpha, nTrain):
+    speciesGroup = data.groupby("Species")
+    
+    #Split nominal points
+    (verTrain, verTest) = sk.model_selection.train_test_split(speciesGroup.get_group("Iris-versicolor"), train_size = nTrain)
+    (virTrain, virTest) = sk.model_selection.train_test_split(speciesGroup.get_group("Iris-virginica"), train_size = nTrain)
+    
+    #anomaly test size will be determined as a percent of the test data (nu)
+    target_aCount = math.ceil((len(verTrain) + len(virTrain))*alpha)
+    aTrain = target_aCount/(len(speciesGroup.get_group("Iris-setosa")))
+    
+    #Split anomalous points
+    (setTrain, setTest) = sk.model_selection.train_test_split(speciesGroup.get_group("Iris-setosa"), train_size = aTrain)
+
+    #Combine corrisponding test and training sets for each species
+    trainData = pd.concat([verTrain, virTrain, setTrain])
+    testData = pd.concat([verTest, virTest, setTest])
+    
+    #Convert Species (3 class) to nominal:1 (vir, ver) and anomaly:-1 (set)
+    trainData = trainData.replace("Iris-versicolor", 1).replace("Iris-virginica", 1).replace("Iris-setosa", -1)
+    testData = testData.replace("Iris-versicolor", 1).replace("Iris-virginica", 1).replace("Iris-setosa", -1)
+    
+    return(trainData, testData)
+
+#buildMatrix(predictions: a numpy array containing the class predicted for each data point, labels: a numpy array containing the "true class" for each data point)
+#By comparing the model prediction with the true value this function decides if the point was correctly classed and categorizes each as:
+#true nominal: correctly labeled as nomial point 
+#false nominal: incorrectly labeled as nomial point
+#true anomaly: correctly labeled as anomaly
+#false anamaly: incorrectly labeled as anomaly
+#
+#returns: numpy array containing one of the above classifications for each point
+def buildMatrix(predictions, labels):
+    if(len(predictions) != len(labels)):
+        print("error: predictions and true values do not match up")
+    cMatrix = []
+    for x in range(0,len(predictions)):
+        if predictions[x] ==  1 and labels[x] == 1:
+            cMatrix.append('true nominal')
+        elif predictions[x] ==  1 and labels[x] == -1:
+            cMatrix.append('false nominal')
+        elif predictions[x] ==  -1 and labels[x] == -1:
+            cMatrix.append('true anomaly')        
+        elif predictions[x] ==  -1 and labels[x] == 1:
+            cMatrix.append('false anomaly')
+        else:
+            print("error: invalid value in array")
+    return cMatrix
+
+#getResults(model: fitted sklearn model to predict data class (1 or -1), scaledData: numpy array or normalized data, trueClass: numpy array containing the "true class"corresponding to each point in the scaledData
+#This function uses the constructed model to pridict the class for each point. It then compares this to the "true class" to classify each point in a confusion matrix
+#
+#returns: pandas dataframe with the the normailzed data columns followed by the predicted class, the "true class", and a confusion matrix classification
+def getResults(model, scaledData, trueClass):
+    prediction = model.predict(scaledData)
+    df = pd.DataFrame(data = scaledData)        
+    df["Prediciton"] = np.atleast_2d(prediction).T
+    df["True Value"] = trueClass
+    df["Confusion Matrix"] = buildMatrix(prediction, trueClass)
+    return df
+
+
+#Step 1) Import data and Choose Presets:
+#Alpha is expected percentage of anomolies in the dataset
+alpha = .03
+#nTrain is percent of nominal points used in the training data 
+nTrain = .5
+
+#Gamma is the kernal width - the array contains the values to test/compare in gridSearch
+gArray = [1.6, 1.4, 1.2, 1, .8, .6, .4, .2, .01, .005]
+#gArray = [1.6, 1.4, 1.2, 1, .8]
+
+#Nu is the upper bound of rejected target data
+nuArray = [.5, .3, .1, .05, .01, .03, .005, .001]
+
+
+
+#Import location
+irisData = pd.read_csv(r"C:\Users\Justin\OneDrive\Desktop\OSU\419\databases\iris.csv")
+
+#Dataframe labels
+sepalCols = ["Sepal length", "Sepal width"]
+dataCols = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
+labelCols = ["Species"]
+
+selectedCols = dataCols
+
+
+
+
+#Step 2)Prepare data and organize into training and testing groups
+trainData, testData = splitData(irisData, alpha, nTrain)
+
+#Step 3: Normalize from training
+#Seperate into data and species label
+#Use Standard scaler to adjust data - fit to training only - transform to test
+#This step also seperated the species column for proper scoring
+scaler = sk.preprocessing.StandardScaler().fit(trainData[selectedCols])
+scaledTrain = scaler.transform(trainData[selectedCols])
+scaledTest = scaler.transform(testData[selectedCols])
+
+
+
+
+
+#Step 4: Fit Model
+#Set up grid search
+oneClass = sk.model_selection.GridSearchCV(sk.svm.OneClassSVM(kernel='rbf'),
+                   param_grid={"nu": nuArray,
+                               "gamma":gArray}, scoring="accuracy")
+
+#Fit the model using the labelCol to score
+oneClass.fit(scaledTrain, trainData[labelCols])
+print("Grid search selected the hyperparameters: ")
+print(oneClass.best_params_)
+
+trainResults = getResults(oneClass, scaledTrain, trainData[labelCols].to_numpy())
+testResults = getResults(oneClass, scaledTest, testData[labelCols].to_numpy())
+
+#Build Plot and Confusion matrix  
+matrixGroups = testResults.groupby("Confusion Matrix")
+colorDict = {'true nominal':'blue', 'false nominal':'red',  'true anomaly':'green', 'false anomaly':'orange'}
+for name, group in matrixGroups:
+    print(name,":",len(group))
+    plt.scatter(group[0], group[1], c = colorDict[name], label = name, alpha = .5)
+
+#Overlay training points for reference
+plt.scatter(trainResults[0], trainResults[1], c = 'black', alpha = 1, s = 10, label = "training")
+
+plt.legend(loc="best")
+plt.show()
+
+
+#Plots first 2 cols (2d plot)
+#plt.scatter(result[:,0],result[:,1], c='y')
+
+
+
+
+
+
+
+
---
+++ ocSVM_hyperParameters.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 13:22:15 2020
+
+@author: Justin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sklearn as sk
+import sklearn.model_selection
+import sklearn.svm
+
+import pandas as pd
+import math
+# Seaborn visualization library
+import seaborn as sns
+
+#Step 1) Import data and Choose Presets:
+#Alpha is expected percentage of anomolies in the dataset
+alpha = .03
+#nTrain is percent of nominal points used in the training data 
+nTrain = .5
+
+#Gamma is the kernal width - the array contains the values to test/compare in gridSearch
+#gArray = [1.6, 1.4, 1.2, 1, .8, .6, .4, .2, .01, .005]
+gArray = [1.6, 1.4, 1.2, 1, .8]
+
+#Nu is the upper bound of rejected target data
+nuArray = [.5, .3, .1, .05, .01, .03, .005, .001]
+
+#Import location
+irisData = pd.read_csv(r"C:\Users\Justin\OneDrive\Desktop\OSU\419\databases\iris.csv")
+speciesGroup = irisData.groupby("Species")
+
+#Dataframe labels
+sepalCols = ["Sepal length", "Sepal width"]
+dataCols = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
+labelCols = ["Species"]
+
+selectedCols = sepalCols
+
+
+
+
+#Step 2)Prepare data and organize into training and testing groups
+(verTrain, verTest) = sk.model_selection.train_test_split(speciesGroup.get_group("Iris-versicolor"), train_size = nTrain)
+(virTrain, virTest) = sk.model_selection.train_test_split(speciesGroup.get_group("Iris-virginica"), train_size = nTrain)
+
+#anomaly test size will be determined as a percent of the test data (nu)
+target_aCount = math.ceil((len(verTrain) + len(virTrain))*alpha)
+aTrain = target_aCount/(len(speciesGroup.get_group("Iris-setosa")))
+(setTrain, setTest) = sk.model_selection.train_test_split(speciesGroup.get_group("Iris-setosa"), train_size = aTrain)
+
+#Combine corrisponding test and training sets for each species
+trainData = pd.concat([verTrain, virTrain, setTrain])
+testData = pd.concat([verTest, virTest, setTest])
+
+#Convert Species (3 class) to nominal:1 (vir, ver) and anomaly:-1 (set)
+trainData = trainData.replace("Iris-versicolor", 1).replace("Iris-virginica", 1).replace("Iris-setosa", -1)
+testData = testData.replace("Iris-versicolor", 1).replace("Iris-virginica", 1).replace("Iris-setosa", -1)
+
+
+
+
+
+#Step 3: Normalize from training
+#Seperate into data and species label
+#Use Standard scaler to adjust data - fit to training only - transform to test
+#This step also seperated the species column for proper scoring
+scaler = sk.preprocessing.StandardScaler().fit(trainData[selectedCols])
+#??? Do I apply the scaler to the training data and the test data???
+scaledTrain = scaler.transform(trainData[selectedCols])
+scaledTest = scaler.transform(testData[selectedCols])
+
+
+
+
+
+#Step 4: Fit Model
+#Set up grid search
+oneClass = sk.model_selection.GridSearchCV(sk.svm.OneClassSVM(kernel='rbf'),
+                   param_grid={"nu": nuArray,
+                               "gamma":gArray}, scoring="accuracy")
+
+#Fit the model using the labelCol to score
+oneClass.fit(scaledTrain, trainData[labelCols])
+print(oneClass.best_params_)
+prediction = oneClass.predict(scaledTest)
+
+#Add predicted value and true value columns back on
+result = np.hstack((scaledTest, np.atleast_2d(prediction).T))
+result = np.hstack((result, testData[labelCols]))
+
+#
+cMatrix = []
+
+
+#Build Plot and Confusion matrix
+trueVal = -1
+predVal = -2
+for sample in result:
+    if sample[trueVal] ==  1 and sample[predVal] == 1:
+        cMatrix.append('tPos')
+    elif sample[trueVal] ==  1 and sample[predVal] == -1:
+         cMatrix.append('fNeg')
+    elif sample[trueVal] ==  -1 and sample[predVal] == -1:
+         cMatrix.append('tNeg')        
+    elif sample[trueVal] ==  -1 and sample[predVal] == 1:
+        cMatrix.append('fPos')
+    else:
+        print("error:", sample)
+
+      
+#result = pd.DataFrame(data = result, columns = ["Sepal length", "Sepal width", "Petal length", "Petal width", "Predicted Value", "True Value"])        
+result = pd.DataFrame(data = result, columns = ["Sepal length", "Sepal width", "Predicted Value", "True Value"])
+#sns.pairplot(result)
+
+result["Confusion Matrix"] = cMatrix
+matrixGroups = result.groupby("Confusion Matrix")
+tPos = matrixGroups.get_group("tPos")
+fPos = matrixGroups.get_group("fPos")
+tNeg = matrixGroups.get_group("tNeg")
+fNeg = matrixGroups.get_group("fNeg")
+plt.scatter(tPos["Sepal length"], tPos["Sepal width"], c = 'g', label = "True Pos")
+plt.scatter(fPos["Sepal length"], fPos["Sepal width"], c = 'r', label = "False Pos")
+plt.scatter(tNeg["Sepal length"], tNeg["Sepal width"], c = 'b', label = "True Neg")
+plt.scatter(fNeg["Sepal length"], fNeg["Sepal width"], c = 'y', label = "False Neg")
+plt.legend()
+plt.show()
+#Plots first 2 cols (2d plot)
+#plt.scatter(result[:,0],result[:,1], c='y')
+
+
+
+
+
+
+
+
